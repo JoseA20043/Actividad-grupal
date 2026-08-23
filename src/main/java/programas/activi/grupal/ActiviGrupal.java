fd1293fb254b97d281aa0a66f7de7fb9434b15c2
JoseA20043 <gomezahumedoa@gmail.com>
2026-08-23 17:30:40 -0500

    Se agrega en trabajo grupal

---
+++ src/main/java/programas/activi/grupal/ActiviGrupal.java
@@ -0,0 +1,8 @@
+package programas.activi.grupal;
+
+public class ActiviGrupal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/programas/activi/grupal/CODIFICACION.java
@@ -0,0 +1,5 @@
+package programas.activi.grupal;
+
+public class CODIFICACION  {
+    
+}
